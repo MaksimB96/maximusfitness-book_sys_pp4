3e94313eb282a4a76205f1d836bf96f2ed2ef634
MaksimB96 <>
2023-04-26 18:16:25 +0000

    Add booking time slots and confirmation status for bookings

--- book/models.py
+++ book/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+# imports
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+
+
+# Time slots for booking
+BOOKING_SLOT = {
+    ("8 AM", "8 AM"),
+    ("10 AM", "10 AM"),
+    ("12 AM", "12 AM"),
+    ("2 AM", "2 AM"),
+    ("4 AM", "4 AM"),
+    ("6 AM", "6 AM"),
+    ("6 AM", "6 AM"),
+}
+
+
+# Confirmation Status of booking
+CONFIRMATION_STATUS = {
+    ("confirmed", "confirmed"),
+    ("declined", "declined"),
+    ("pending", "pending"),
+}
