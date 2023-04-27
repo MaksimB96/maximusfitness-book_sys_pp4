a1e310fc07f377176fecb4e8bebb8c97b9c16883
MaksimB96 <>
2023-04-27 10:27:22 +0000

    Add additional item to model, removed content block

--- book/admin.py
+++ book/admin.py
@@ -3,6 +3,4 @@ from .models import Booking
 from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(Booking)
-class BookingAdmin(SummernoteModelAdmin):
-    summernote_fields = ('client_req')
+
--- book/models.py
+++ book/models.py
@@ -13,7 +13,14 @@ BOOKING_SLOT = {
     ("2 PM", "2 PM"),
     ("4 PM", "4 PM"),
     ("6 PM", "6 PM"),
-    ("6 PM", "6 PM"),
+    ("8 PM", "8 PM"),
+}
+
+
+SERVICES_CHOICE = {
+    ("One on One", "One on One"),
+    ("Consultation", "Consultation"),
+    ("PT Package", "PT Package"),
 }
 
 
@@ -31,10 +38,19 @@ class Booking(models.Model):
     """
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    service = models.CharField(max_length = 50,
+     choices=SERVICES_CHOICE, default='One on One'
+     )
     day = models.DateField(default=datetime.now)
-    time = models.CharField(max_length=10, choices=BOOKING_SLOT, default='8 AM')
-    client_req = models.TextField()
-    confirm_status = models.CharField(max_length=10, choices=CONFIRMATION_STATUS, default='pending')
+    time = models.CharField(max_length=10,
+     choices=BOOKING_SLOT, default='8 AM'
+     )
+    confirm_status = models.CharField(max_length=10,
+     choices=CONFIRMATION_STATUS, default='pending'
+     )
+
+    class Meta:
+        ordering = ['day']
 
     def __str__(self):
-        return f'User: {self.user} Date: {self.day} Time: {self.time} Status: {self.confirm_status}'
+        return f'User: {self.user} | Date: {self.day} | Time: {self.time} | Status: {self.confirm_status}'
