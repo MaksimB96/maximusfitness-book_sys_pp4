91ae45cbff21a9a24f4a05c65b77a6146fa1c095
MaksimB96 <>
2023-04-28 12:03:02 +0000

    Remove base nav bar html as was placeholder

--- book/admin.py
+++ book/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
+from .models import SessionBook
 from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(SessionBook)
+class AdminSession(SummernoteModelAdmin):
+
+    list_filter = ('confirmed', 'sent_on')
+    summernote_fields = ('client_notes')
--- book/models.py
+++ book/models.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 
 # Time slots for booking
-BOOKING_SLOT = {
+SLOT = {
     ("8 AM", "8 AM"),
     ("10 AM", "10 AM"),
     ("12 PM", "12 PM"),
@@ -15,3 +15,19 @@ BOOKING_SLOT = {
     ("6 PM", "6 PM"),
     ("8 PM", "8 PM"),
 }
+
+
+class SessionBook(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    booked_slot = models.CharField(max_length=10, choices=SLOT, default='8 AM')
+    client_notes = models.TextField(blank=True) 
+    sent_on = models.DateField(default=datetime.now)
+    confirmed = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-sent_on']
+
+    def __str__(self):
+        return self.user
