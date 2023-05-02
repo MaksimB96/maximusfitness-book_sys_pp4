664646937876a7fddb4f00388abe92988dd9a357
MaksimB96 <>
2023-05-02 13:58:03 +0000

    Fix scroll effects, removed owl-carousel

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,12 @@
-// const ScrollReveal = require('scrollreveal');
 
-// ScrollReveal().reveal('.left-slide-text');
+ScrollReveal().reveal('.left-slide-text', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
 
-// ScrollReveal().reveal('.right-slide-img');
+ScrollReveal().reveal('.right-slide-img', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
