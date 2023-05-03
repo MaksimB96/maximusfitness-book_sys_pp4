9b90e903c02639b655d09bd752688e7ebb595bff
MaksimB96 <>
2023-05-03 16:06:05 +0000

    Add final animations to index.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,27 @@
+ScrollReveal().reveal('.navbar-brand', {
+    duration: 2000,
+    origin: 'bottom'
+});
 
 ScrollReveal().reveal('.left-slide-text', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px'
+});
+
+ScrollReveal().reveal('.right-slide-img', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px'
+});
+
+ScrollReveal().reveal('.responsive-map', {
     duration: 2000,
     origin: 'left',
     distance: '300px'
 });
 
-ScrollReveal().reveal('.right-slide-img', {
+ScrollReveal().reveal('.right-slide-text', {
     duration: 2000,
     origin: 'right',
     distance: '300px'
