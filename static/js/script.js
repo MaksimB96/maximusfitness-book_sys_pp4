34a70e79422fcb0155b3cb146b60146498b45bee
MaksimB96 <>
2023-05-05 10:25:58 +0000

    Add Scroll reveal to contact area

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,14 @@
-// Functions to add scroll effects to site
+/**
+ * Scroll reveal utilizes npm to create on scroll interactivity
+ */
 
+// Nav-Logo
 ScrollReveal().reveal('.navbar-brand', {
     duration: 2000,
     origin: 'bottom'
 });
 
+// About Section - Index
 ScrollReveal().reveal('.left-slide-text', {
     duration: 2000,
     origin: 'bottom',
@@ -29,6 +33,7 @@ ScrollReveal().reveal('.right-slide-text', {
     distance: '300px'
 });
 
+// Carousel - Index
 ScrollReveal().reveal('.tester_new', {
     duration: 2000,
     origin: 'left',
@@ -36,4 +41,37 @@ ScrollReveal().reveal('.tester_new', {
     delay: 1000
 });
 
+// Contact us - Index
+ScrollReveal().reveal('.con-name', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+});
+
+ScrollReveal().reveal('.con-email', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    delay: 250
+});
+
+ScrollReveal().reveal('.con-text', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    delay: 500
+});
+
+ScrollReveal().reveal('.con-submit', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    delay: 750
+});
+
+ScrollReveal().reveal('.map-area', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px',
+});
 
