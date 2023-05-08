7bce066a10a6ebaaf92d6969368a6f280da83b04
MaksimB96 <>
2023-05-08 09:32:35 +0000

    Add more robust model

--- book/forms.py
+++ book/forms.py
@@ -1,3 +1,16 @@
 from .models import SessionBook
-from django import crispy_forms
+from django.forms import ModelForm
+from django import forms
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class TimeInput(forms.TimeInput):
+    input_type = 'text'
+
+
+class CreateBooking(forms.ModelForm):
+    class Meta:
+        model = SessionBook
+        fields = ('')
--- book/models.py
+++ book/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from datetime import datetime
 
 
@@ -20,11 +21,23 @@ SLOT = {
 class SessionBook(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, null=True, blank=True)
+    fname = models.CharField(max_length=15, blank=True)
+    lname = models.CharField(max_length=15, blank=True)
+    age = models.IntegerField(
+        null=False,
+        blank=False,
+        default=18,
+        validators=[
+            MinValueValidator(18),
+            MaxValueValidator(80)
+        ]
+    )
     email = models.EmailField()
     phone = models.CharField(max_length=15)
     booked_slot = models.CharField(max_length=10, choices=SLOT, default='8 AM')
     client_notes = models.TextField(blank=True)
     sent_on = models.DateField(default=datetime.now)
+    update_on = models.DateTimeField(auto_now=True, blank=True)
     confirmed = models.BooleanField(default=False)
 
     class Meta:
@@ -32,4 +45,3 @@ class SessionBook(models.Model):
 
     def __str__(self):
         return self.user
-
