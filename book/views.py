6c05716d8b339fc82e3fd3271bc89531fa07537b
MaksimB96 <>
2023-05-13 19:48:12 +0000

    Add adjustments to mobile

--- book/views.py
+++ book/views.py
@@ -59,6 +59,7 @@ def get_session(request):
     """
     user = request.user
     items = SessionBook.objects.filter(user=user)
+    
     return render(request, 'manage-session.html', {
         'user': user,
         'items': items,
