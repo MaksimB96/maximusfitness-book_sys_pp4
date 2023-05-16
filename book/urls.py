64119004f6fedc1f1fc3425004fc9d6414147b64
MaksimB96 <>
2023-05-16 23:27:19 +0000

    Add url adjustment

--- book/urls.py
+++ book/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.HomeTemplate.as_view(), name='home'),
+    path("", views.index_view, name='home'),
     path("book-session/", views.book_session, name='book-session'),
     path("manage-session/", views.get_session, name='manage-session'),
     path("update-session/<id>", views.update_session, name='update-session'),
