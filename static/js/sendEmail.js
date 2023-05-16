218453216d187d4375abda51515d1c3efb6c193a
MaksimB96 <>
2023-05-16 23:21:03 +0000

    Fix email JS functionality

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,14 +1,17 @@
-function sendMail(contactForm){
-    emailjs.send("service_eq1uv2d","template_2e9pylo", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "message": contactForm.message.value
-    })
+function sendEmail(){
+
+    const templateParams = {
+        "from_name": document.getElementById("name").value,
+        "from_email": document.getElementById("email").value,
+        "message": document.getElementById("text_area").value
+    };
+
+    emailjs.send("service_eq1uv2d","template_2e9pylo", templateParams)
     .then(
         function(response) {
-            console.log("Success", response)
+            console.log("Success", response);
         },
         function(error){
-            console.log("Error", error)
-        })
+            console.log("Error", error);
+        });
 }
