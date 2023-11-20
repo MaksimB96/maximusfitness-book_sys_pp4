b3c78c2d8c4c65ab63475473ce9d45f8a323cb96
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-11-20 15:11:58 +0000

    Add: changes to settings

--- maximus_book/settings.py
+++ maximus_book/settings.py
@@ -80,7 +80,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'maximus_book.urls'
